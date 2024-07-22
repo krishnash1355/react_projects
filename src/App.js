edff31f7c73036342a2ed48ecff255c5163d59a2
Krishna Sharma <krishnacoder1355@gmail.com>
2024-07-22 21:52:55 +0530

    Image Slider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import './App.css';
-import Accordion from './Components/accordian';
-import RandomColor from './Components/random-color';
-import Star from './Components/star_rating';
+import "./App.css";
+import Accordion from "./Components/accordian";
+import ImageSlider from "./Components/image-slider";
+import RandomColor from "./Components/random-color";
+import Star from "./Components/star_rating";
 
 function App() {
   return (
     <div className="App">
-    {/* Accordian */}
-    <Accordion>Aaja</Accordion>
-    {/* Random Color Component */}
-    <RandomColor></RandomColor>
-    {/* star Rating */}
-    <Star></Star>
+      {/* Accordian */}
+      <Accordion>Aaja</Accordion>
+      {/* Random Color Component */}
+      <RandomColor></RandomColor>
+      {/* star Rating */}
+      <Star></Star>
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
